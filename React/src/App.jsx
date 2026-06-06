00d48e55d497720c29d7aef14e08f0546b28ea44
abdul Rafay <abdulrafay700000@gmail.com>
2026-06-06 09:36:41 +0500

    fixed folder not opening on github

---
+++ React/src/App.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+// Acha Ab React me Hamhe html bhi react me he likhni hoti he thk hena JSX Syntex bolte  hen
+// Or isme Aik Function me html return hota he or isme Function me Aik jesi do chezze 
+// Pass nhi hote he jese 
+// return ( 
+//          <h1> H1 tag </h1>
+//          <h1> Wapis Se H1 Tag </h1> 
+//   Ab ye Error dega Q ke do h1 return horahe hen iske liye use hota he <> </>
+//   iske ander aik jese html tag likh skhte ho or <div></div> ke ander bhi 
+//   Aik jesi html likh skhte ho 
+//          
+// )
+
+const App = () => {
+  return (
+    <>
+    <div>
+          <h1>App</h1>
+          <h2>Jeo</h2>
+    </div>
+    <div>
+          <h1>App</h1>
+          <h2>Jeo</h2>
+    </div>
+    </>
+  )
+}
+
+export default App
+
+
