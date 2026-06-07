3dc38a4d0fb0ea4a84e3376ca626d5d9f28454ea
abdul Rafay <abdulrafay700000@gmail.com>
2026-06-07 08:53:02 +0500

    Learn Components

---
+++ 02-components/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from './Navbar.jsx'
+import Card from './Card.jsx'
+
+
+const App = () => {
+  return (
+     <div>
+           <Navbar />
+           <Card />
+     </div>
+  )
+}
+
+export default App
---
+++ 02-components/src/Card.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div className='component1'>
+          Today I learn Component in react.js from Sheriyans coding he is a best for
+          Learn Coding          
+    </div>
+  )
+}
+
+export default Card
+
+
