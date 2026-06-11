c89992bda7f6fc45c1b479b868768549fe7b1f4a
abdul Rafay <abdulrafay700000@gmail.com>
2026-06-11 10:55:23 +0500

    Two Ways Binding

--- 07-Hooks/src/formHandling.jsx
+++ 07-Hooks/src/formHandling.jsx
@@ -1,4 +1,5 @@
     import React from 'react'
+    import { useState } from 'react';
 
     const formHandling = () => {
 
@@ -7,13 +8,37 @@
             console.log("Form Working Without reload")
         }
 
+        // Two Way Binding ------------- Start
+
+        // Tow Ways binding me yahi hota he jaab hamhe kuch change krna hoto 
+        // Khud nhi krte hen React ko bolte hen wo krta he jese hamne is code me kara he
+        // Pehle kahli input rkha const input me phr jese input change horaha he 
+        // setinput(e.target.value) wese he yaha wo value  set horahi he or 
+
+        const [input, setinput] = useState('')
+
+        // Two Way Binding ------------- End
+
+        
+
 
     return (
         <div className="bg-yellow-700 p-20">
         <form onSubmit={(e) => {
             change(e)
         }}>
-            <input type="text" placeholder='Names' className='p-2'></input>
+            <input
+                    type="text" 
+                    placeholder='Names' 
+                    className='p-2'
+                    // Two Way Binding -------------- Start
+                    value={input}
+
+                    onChange={(e) => {
+                        setinput(e.target.value)
+                    }}
+                    // Two Way Binding -------------- End
+            ></input>
             <br></br>
             <br></br>
             <button className='bg-red-900 text-white w-32 h-10'>Submit</button>
