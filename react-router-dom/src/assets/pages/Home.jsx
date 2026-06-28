7439e66e24a493388125823e9b02920c6ac0cde6
abdul Rafay <abdulrafay700000@gmail.com>
2026-06-28 23:55:13 +0500

    Navigate implement

--- react-router-dom/src/assets/pages/Home.jsx
+++ react-router-dom/src/assets/pages/Home.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
+
+
 
 const Home = () => {
+  const navigate = useNavigate()
   return (
     <div className='Home'>
+          <button className='AboutPageNavigate' onClick={() => {
+             navigate('/About')
+          }}>About Page</button>
           <h1>Home Page</h1>
     </div>
   )
